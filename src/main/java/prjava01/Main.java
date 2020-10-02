86515c9b781604e7a1e46e22920923eab7e6a1bc
pogrpi2021daw2@gmail.com <PolGR@DESKTOP-1SD8CK3>
2020-10-02 17:23:47 +0200

    Versió 0.2 del projecte prjava01

--- src/main/java/prjava01/Main.java
+++ src/main/java/prjava01/Main.java
@@ -13,19 +13,20 @@ public class Main {
  * @param args the command line arguments
  */
  public static void main(String[] args) throws IOException {
- File f = new File("fitxer.html");
- try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
- bw.write("<html>");bw.newLine();
- bw.write(" <head>");bw.newLine();
- bw.write(" <title>");bw.newLine();
- bw.write(" Nova p&agrave;gina web");bw.newLine();
- bw.write(" </title>");bw.newLine();
- bw.write(" </head>");bw.newLine();
- bw.write(" <body>");bw.newLine();
- bw.write(" Nova p&agrave;gina web");bw.newLine();
- bw.write(" </body>");bw.newLine();
- bw.write("</html>");bw.newLine();
- bw.close();
- }
- }
+    File f = new File("fitxer.html");
+    try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+        bw.write("<html>");bw.newLine();
+        bw.write(" <head>");bw.newLine();
+        bw.write(" <title>");bw.newLine();
+        bw.write(" Nova p&agrave;gina web");bw.newLine();
+        bw.write(" </title>");bw.newLine();
+        bw.write(" </head>");bw.newLine();
+        bw.write(" <body>");bw.newLine();
+        bw.write(" Nova p&agrave;gina web");bw.newLine();
+        bw.write(" </body>");bw.newLine();
+        bw.write(" <h1>DAW2 m08uf4pr2</h1>");bw.newLine();
+        bw.write("</html>");bw.newLine();
+        bw.close();
+        }
+    }
 }
